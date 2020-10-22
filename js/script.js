6ba559b215843a33b5fded5fb6706d7b7609b79f
gorgeous-skill-0664 <wbugenis@gmail.com>
2020-10-22 05:43:54 +0000

    Automatically backed up by Learn

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>  
 $(document).ready(function() {
    $("#text").append("this is so freaking cool.");
 });
